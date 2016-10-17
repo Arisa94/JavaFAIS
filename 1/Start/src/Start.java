900620b00a747e5d98af323a3b244fe3b0c5c094
Patrycja Brzeska <Patrycja Brzeska>
2016-10-17 16:12:54 +0200

    initial commit

---
+++ 1/Start/src/Start.java
@@ -0,0 +1,69 @@
+import static java.lang.Math.sqrt;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Start{
+
+    public static final double START = 1.0, FINISH = 123.4, STEP = 0.5, STEP_ADD = 0.2;
+
+    public static void sum(double[] tab){
+
+        double sum = 0;
+
+        for(int i = 0; i < tab.length; i++){
+
+            sum += tab[i];
+
+        }
+
+        System.out.println("suma = " + sum);
+        average(tab, sum);
+
+    }
+
+    public static void average(double[] tab, double sum){
+
+        double average = sum / tab.length;
+        System.out.println("srednia = " + average);
+        standardDeviation(tab, average);
+
+    }
+
+    public static void standardDeviation(double[] tab, double average){
+
+        double standardDeviation;
+        double temp = 0;
+        int N = tab.length;
+
+        for(int i = 0; i < N; i++){
+
+            temp += ((average - tab[i]) * (average - tab[i])) / (N * (N - 1));
+
+        }
+
+        standardDeviation = sqrt(temp);
+        System.out.println("odchylenie std. sredniej = " + standardDeviation);
+
+    }
+
+    public static void main(String[] args){
+
+        List<Double> numbers = new ArrayList<>();
+        double TEMP = START;
+        double TEMP2 = STEP;
+        numbers.add(START);
+
+        while(TEMP + TEMP2 <= FINISH){
+
+            TEMP += TEMP2;
+            numbers.add(TEMP);
+            TEMP2 += STEP_ADD;
+
+        }
+
+        double[] tab = numbers.stream().mapToDouble(Double::doubleValue).toArray();
+        sum(tab);
+
+    }
+
+}
